237761ef4ae9ac35bf8f3a59841ee0eb70ea753d
Lance de Guzman <lance.deguzman@countable.ca>
2026-09-21 17:19:46 +0800

    fix: download backend

--- core/settings.py
+++ core/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://frontend-prpa3bfc2-lancedgzmans-projects.vercel.app",
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -128,7 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
